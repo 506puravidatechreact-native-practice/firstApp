36b126ea5fca604e6d95d1d4732d9fbdda038b84
Manuel Herrera Mesén <mob.manu.herrera.mesen@gmail.com>
2021-06-23 20:13:46 -0600

    second one commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 /**
  * @format
  */
+
+//importatr las librerias
 import React from 'react';
 import {Text, AppRegistry} from 'react-native';
 
+//create component
 const App = () => {
   return <Text>Hi there</Text>;
 };
 
+//make a component avaibale to other component
 AppRegistry.registerComponent('firstApp', () => App);
