b931eceb9dfc1f33316451171d826df70e4c347a
Manuel Herrera Mesén <mob.manu.herrera.mesen@gmail.com>
2021-06-23 19:58:13 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+
+const App = () => {
+  return <Text>Hi there</Text>;
+};
+
+AppRegistry.registerComponent('firstApp', () => App);
